97078ae681e837eef7a3ed57e467261699ddbd4b
Ermin Lilaj <ermin.lilaj04@gmail.com>
2024-08-05 22:23:11 +0200

    add product w img

--- ecom-proj/ecom-proj/src/main/java/com/example/ecom_proj/service/ProductService.java
+++ ecom-proj/ecom-proj/src/main/java/com/example/ecom_proj/service/ProductService.java
@@ -3,8 +3,11 @@ package com.example.ecom_proj.service;
 import com.example.ecom_proj.model.Product;
 import com.example.ecom_proj.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,5 +22,14 @@ public class ProductService {
 
     public Product getProductbyId(int id) {
         return repo.findById(id).orElse(null);
+    }
+
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+
+       return repo.save(product);
     }
 }
